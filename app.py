ba79c42c74bca565f9ba1f850601c0612711c0f4
VarmaSource <87811836+VarmaSource@users.noreply.github.com>
2021-07-23 00:28:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pytesseract
 from PIL import Image
 pytesseract.pytesseract.tesseract_cmd = '/app/.apt/urs/bin/tesseract'
+#hi
 st.title("Minor Project")
 img=st.sidebar.file_uploader("Choose an image")
 if img is not None:
